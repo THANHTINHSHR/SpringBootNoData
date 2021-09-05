6d7a8ec6cd47545f092c2ea4884487c022109f0c
THANHTINHSHR <thanhtinh14.06.1998@gmail.com>
2021-09-05 14:11:34 +0700

    first commit

---
+++ src/main/java/com/example/springbootnodata/SpringBootNoDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootnodata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNoDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootNoDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootnodata/SpringBootNoDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootnodata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNoDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
